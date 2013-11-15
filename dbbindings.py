9137c508da9dc841be4c1853cfbe729073ba3927
Matt Nicklay <matthew.nicklay@gmail.com>
2013-11-15 06:30:21 -0600

    Added date and remote IP to data logged in /var/log/presidio/log.txt
    FWIW, this will enable marginally better analytics.

--- dbbindings.py
+++ dbbindings.py
@@ -2,13 +2,16 @@
 
 #So we load in the terms that allow the API implementation to happen for now.
 execfile("APIimplementation.py")
-
+from datetime import datetime
+import os
 import cgitb
 cgitb.enable()
 
 def headers(method):
     if method!="return_tsv":
         print "Content-type: text/html\n"
+        userip = cgi.escape(os.environ["REMOTE_ADDR"])
+        return userip
     elif method=="return_tsv":
         print "Content-type: text; charset=utf-8"
         print "Content-Disposition: filename=Bookworm-data.txt"
@@ -26,8 +29,8 @@ form = cgi.FieldStorage()
 
 if len(form) > 0: #(Use CGI input if it's there:)
     JSONinput = form["queryTerms"].value
-    outfile.write(JSONinput)
-    outfile.write("\n")
+    data = json.loads(JSONinput)
+    data['log_date'] = datetime.now().strftime('%d/%h/%Y:%H:%M:%S')
     output = open("/tmp/err",'w'); output.write(json.__file__)
     data = json.loads(JSONinput)
     #For back-compatability, "method" can be defined in the json or as a separate part of the post.
@@ -41,7 +44,11 @@ if len(form) > 0: #(Use CGI input if it's there:)
         except:
             raise
             pass
-    headers(method)
+    data['user_ip'] = headers(method)
+    outfile.write('%s\n' % json.dumps(data))
+    outfile.flush()
+    for userinfo in ['user_ip', 'log_date']:
+        data.pop(userinfo)
 
     #if somewhere else has already set a privileges level, then you can get higher ones here.
     try:
